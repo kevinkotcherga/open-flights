03c6628df95a12beb3bf5d9689f11fc0fa4b8e02
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-15 16:47:17 +0100

    ✨feat(API): l'api crud pour les compagnies et l'api pour laisser des avis sur les compagnies a été testé sur 'insomnia'

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,14 +1,14 @@
 module Api
   module V1
     class ReviewsController < ApplicationController
-
+      protect_from_forgery with: :null_session
       def create
         review = Review.new(review_params)
 
         if review.save
           render json: ReviewSerializer.new(review).serialized_json
         else
-          render json: { error: review.errors.messages }, status 422
+          render json: { error: review.errors.messages }, status: 422
         end
       end
 
@@ -18,7 +18,7 @@ module Api
         if review.destroy
           head :no_content
         else
-          render json: { error: review.errors.messages }, status 422
+          render json: { error: review.errors.messages }, status: 422
         end
       end
 
